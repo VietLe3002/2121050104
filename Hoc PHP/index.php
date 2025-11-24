0cf3fbbd4feef73c3685a74c0b5fcb9ddcefd364
VietLe3002 <vietle3002@gmail.com>
2025-11-24 15:53:23 +0700

    html,css,jvs,php

---
+++ Hoc PHP/index.php
@@ -0,0 +1,62 @@
+<!-- 
+
+
+
+
+// define('SITE_NAME' , 'VA.vn');
+
+// echo SITE_NAME;
+
+
+// const SITE_NAME = 'VIETANH.VN';
+
+// echo SITE_NAME;
+
+
+// Kiểu dữ liệu string 
+
+// $va1 = 'Vad.vn';
+// $va2 = "Trang web: $va1";
+
+
+// echo $va2;
+
+
+
+// Kiểu số nguyên
+// $bien1 = 5;
+
+// var_dump($bien1);
+
+
+
+// $bien1 = 5;
+
+// $bien2 = 3.7;
+
+// var_dump($bien2); 
+
+
+
+// TỔNG QUAN MYSQL và ngôn ngữ truy vấn
+
+// Client(người dùng) -> request -> server(xử lý) -> CSDL (lấy dữ liệu) -> respon data -> client
+
+// Ngôn ngữ truy vấn: SQL (ko thay đổi)
+
+// MySQL
+
+
+// Kiểu dữ liệu SQL:
+
+// + char: chuỗi có độ dài cố định
+
+// + vachar: chuỗi có độ dài biến đổi.
+
+// + text: chuỗi có độ dài ko quá 65,535 byte
+
+// + INT: Số nguyên
+
+// + datetime: ngày và giờ
+
+// + date: ngày -->
---
+++ bai13 PHP/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài1</title>
+</head>
+<body>
+     
+    <?php 
+        
+        //1. Cú pháp in màn hình
+
+        echo "Hello World <br>";
+ 
+        echo "PHP <br>";
+
+        // 2. Biến
+
+        $Ten = "Viet Anh";
+        $Tuoi = "29";
+
+        echo "Xin chào". $Ten . "Bạn Năm nay" . $Tuoi ."Tuổi <br>" ;
+
+        
+
+        // 3. Hằng số
+
+        define ("SoPI" , "3.14");
+        echo SoPI . "<br>";
+
+
+        //4. phân biệt " " và ' '
+
+        echo '$Ten' . "<br>";
+        echo "$Ten" . "<br>";
+
+
+        // 5. Chuỗi
+        // 5.1 ktra độ dài chuỗi
+
+        echo strlen($Ten)  . "<br>";
+
+
+        // 5.2 Đếm số từ
+        echo str_word_count($Ten) . "<br>";
+
+
+        // 5.3 tìm kiếm ký tự trong chuỗi
+        echo strpos($Ten, "A") . "<br>";
+
+        // 5.4 Thay thế kí tự trong chuỗi
+        echo str_replace("Anh" , "An" , $Ten) . "<br>";
+ 
+        // 6.Toán tử
+        $soThuNhat = 10;
+        $soThuHai = 6;
+        
+        $Tong = $soThuNhat + $soThuHai;
+
+        // + - * / %
+        // += -= *= /=
+        // so sánh == != > < >= <= ===
+
+        echo $soThuNhat %= $soThuHai;
+
+
+        //7. Câu điều kiện
+
+        // ktra tổng số thứ 1 và số thứ 2
+        // Nếu < 15 , Thì hthi tổng là ... nhỏ hơn 15
+        // Nếu = 15 , Thì hthi tổng là ... nhỏ hơn 15
+        // Nếu > 15 , Thì hthi tổng là ... nhỏ hơn 15
+
+
+       if($Tong < 15){
+        echo "Tổng nhỏ hơn 15";
+
+       } elseif($Tong == 15){
+        echo "Tổng bằng 15";
+
+       }else{
+        echo "Tổng lớn hơn 15";
+       }
+
+
+       //8. switch case
+       $color = "red";
+       switch ($color){
+          case "red":
+            echo "is red";
+            break;
+        case "blue":
+            echo "is blue";
+            break;
+        default:
+            echo "no color";
+            break;
+       }
+
+
+       // 9. For
+       for ($i = 0;  $i < 100;  $i++){
+        echo $i . "<br>";
+       }
+    
+
+
+    
+    
+    ?>
+
+</body>
+</html>
